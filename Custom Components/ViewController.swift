69d4264f0b9b53f925c55924fa45d20f717b9e48
nkkha.dev <45230159+nkkha@users.noreply.github.com>
2024-08-09 11:39:11 +0700

    Initial Commit

---
+++ Custom Components/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Custom Components
+//
+//  Created by Kha Nguyen on 9/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
